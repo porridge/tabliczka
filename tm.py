06e60f6a10ad77d8c84712e07de60606daafa17a
Marcin Owsiany <marcin@owsiany.pl>
2021-10-06 16:26:38 +0200

    Get rid of Answer class.

--- tm.py
+++ tm.py
@@ -23,8 +23,8 @@ def main():
     state = State()
     while True:
         problem = state.generate_problem()
-        answer = ui.show_problem_and_get_answer(problem)
-        state.update_from(problem, answer)
+        ui.show_problem_and_get_answer(problem)
+        state.update_from(problem)
         state.save()
 
 
@@ -42,12 +42,12 @@ class State:
             logging.warning('Failed to load state, creating empty state: %s' % e)
             self._frequency_map = dict((q, _FREQ_UNKNOWN) for q in itertools.product(_NUMBERS, _NUMBERS))
     
-    def update_from(self, problem, answer):
+    def update_from(self, problem):
         q = problem._question()
         # TODO: take historical data into account as well
-        if not problem.was_correct(answer):
+        if not problem.answered_correctly():
             self._frequency_map[q] = _FREQ_UNKNOWN
-        elif answer.delay() <= _QUICK_ANSWER_SEC:
+        elif problem.answer_delay() <= _QUICK_ANSWER_SEC:
             self._frequency_map[q] = _FREQ_QUICK
         else:
             self._frequency_map[q] = _FREQ_SLOW
@@ -70,7 +70,7 @@ class CLI:
     def show_problem_and_get_answer(self, problem):
         print(problem)
         asked_time = time.time()
-        return Answer(input(), asked_time)
+        problem.answered(input(), asked_time)
 
 
 class GUI:
@@ -85,7 +85,7 @@ class GUI:
         return self
 
     def show_problem_and_get_answer(self, problem):
-        return Answer()
+        problem.answered("", time.time())
 
         while True:
             for event in pygame.event.get():
@@ -113,7 +113,7 @@ class Problem:
         return (self._a, self._b)
 
     def correct_answer(self):
-        return self._a * self._b
+        return str(self._a * self._b)
 
     def answers(self):
         answers = list(random.sample(self.wrong_answers().keys(), 3)) + [self.correct_answer()]
@@ -124,18 +124,25 @@ class Problem:
         # TODO: also generate correct_answer+1, +2, -1, -2, ...
         closest_as = closest_ns(self._a)
         closest_bs = closest_ns(self._b)
-        closest_problems = dict((p[0]*p[1], p) for p in itertools.product(closest_as, closest_bs))
+        closest_problems = dict((str(p[0]*p[1]), p) for p in itertools.product(closest_as, closest_bs))
         del closest_problems[self.correct_answer()]
         if len(closest_problems) >= 3:
             return closest_problems
         close_as = close_ns(self._a)
         close_bs = close_ns(self._b)
-        close_problems = dict((p[0]*p[1], p) for p in itertools.product(close_as, close_bs))
+        close_problems = dict((str(p[0]*p[1]), p) for p in itertools.product(close_as, close_bs))
         del close_problems[self.correct_answer()]
         return close_problems
 
-    def was_correct(self, answer):
-        return self.correct_answer() == answer.number()
+    def answered(self, answer_text, asked_time):
+        self._answer_text = answer_text.strip()
+        self._answer_delay = time.time() - asked_time
+
+    def answer_delay(self):
+        return self._answer_delay
+
+    def answered_correctly(self):
+        return self._answer_text == self.correct_answer()
 
 
 def closest_ns(n):
@@ -155,17 +162,6 @@ def close_ns(n):
     else:
         return n-2, n-1, n, n+1, n+2
 
-
-class Answer:
-    def __init__(self, text, asked_time):
-        self._text = text
-        self._delay = time.time() - asked_time
-
-    def delay(self):
-        return self._delay
-
-    def number(self):
-        return int(self._text)
 
 
 if __name__ == '__main__':
