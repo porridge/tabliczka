737193fc07cbeac69b54cf948016ac09e7fd2741
Marcin Owsiany <marcin@owsiany.pl>
2021-11-03 16:28:30 +0100

    Added license.

--- tabliczka.py
+++ tabliczka.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+# tabliczka: a program for learning multiplication table
+# Copyright 2021 Marcin Owsiany <marcin@owsiany.pl>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
 import argparse
 import itertools
 import logging
