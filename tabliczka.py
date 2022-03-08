b4c786c155f4eec263366e5076851ec73e4e1fc2
Marcin Owsiany <marcin@owsiany.pl>
2022-03-08 21:57:03 +0100

    Disable showing feedback by default.

--- tabliczka.py
+++ tabliczka.py
@@ -57,13 +57,14 @@ def main():
     parser.add_argument('--dump', action='store_true', help='Just show the saved state and quit.')
     parser.add_argument('--limit', type=int, help='Quit after correctly solving this many questions.')
     parser.add_argument('--show_scores', action='store_true', help='Show scores in main window.')
+    parser.add_argument('--show_feedback', action='store_true', help='Show feedback on wrong answers.')
     args = parser.parse_args()
 
     if args.dump:
         State.load().dump()
         return
 
-    with get_ui_class(args.ui)(args.show_scores) as ui:
+    with get_ui_class(args.ui)(args.show_scores, args.show_feedback) as ui:
         try:
             run(ui, args.limit)
         except QuitException:
@@ -177,7 +178,7 @@ class State:
 
 
 class CLI:
-    def __init__(self, show_scores):
+    def __init__(self, show_scores, show_feedback):
         pass
 
     def __enter__(self):
@@ -203,10 +204,11 @@ class GUI:
     _answer_correct_color = pygame.Color('lightgreen')
     _answer_error_color = pygame.Color(238, 144, 144, 255)
 
-    def __init__(self, show_scores):
+    def __init__(self, show_scores, show_feedback):
         self._font_size = 80
         self._score_font_size = 50
         self._should_show_scores = show_scores
+        self._should_show_feedback = show_feedback
 
     def __enter__(self):
         pygame.init()
@@ -248,6 +250,8 @@ class GUI:
                     return
 
     def provide_feedback(self, problem, state):
+        if not self._should_show_feedback:
+            return
         self._display_problem(problem, state, reveal_solution=True)
         wait_start = pygame.time.get_ticks()
         wait_end = wait_start + _ERROR_FEEDBACK_DELAY_MILLISEC
